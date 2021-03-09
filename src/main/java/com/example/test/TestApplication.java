b3a0474b9e3b09b912d77da67c65b8fb5ebdc8e7
zizou98 <azizbenyaala@gmail.com>
2021-03-09 14:30:23 +0100

    swagger

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -2,8 +2,11 @@ package com.example.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+
 public class TestApplication {
 
     public static void main(String[] args) {
